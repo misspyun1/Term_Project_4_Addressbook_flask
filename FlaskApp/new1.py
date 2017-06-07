01503af713015be9911719d53d13e84308b1f1c2
KKANG00 <wldn7238@naver.com>
2017-06-07 19:11:26 +0900

    db connection

--- FlaskApp/new1.py
+++ FlaskApp/new1.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect,request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///addressbook.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+db=SQLAlchemy(app)
+
+class PERSON(db.Model):
+   id=db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+   name=db.Column(db.String(50), nullable=False, default='')
+   number=db.Column(db.String(50), nullable=False, default='')
+   email=db.Column(db.String(50), nullable=False, default='')
+
+   def __init__(self, name, number, email):
+      self.name=name
+      self.number=number
+      self.email=email
+
 
 @app.route("/")
-def new1():
-	return render_template('index.html')
-	
-if __name__ == "__main__" :
-	app.run()
+def main():
+	return render_template('index.html', ADDRESSBOOK=PERSON.query.all())
 
+@app.route("/new", methods=['GET', 'POST'])
+def new():
+	 if request.method == 'POST':
+	 	if not request.form['name'] or not request.form['number'] or not request.form['email']:
+	 		flash('Please enter all the fields', 'error')
+	 	else:
+	 		NEW=PERSON(request.form['name'],request.form['number'],request.form['email'])
+	 		db.session.add(NEW)
+	 		db.session.commit()
+	 		return redirect("/")
+	 return render_template('plus.html')
+
+if __name__ == "__main__" :
+	db.create_all()
+	app.run(debug=True)
